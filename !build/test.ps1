2414ba099dbe9fbce65e3a816a678f4a7b8b45c9
jannelang <118393059+jannelang@users.noreply.github.com>
2023-12-11 14:15:39 +0200

    Update test.ps1

--- !build/test.ps1
+++ !build/test.ps1
@@ -1,1 +1,2 @@
-az bicep version
+Remove-Item -Path .\Bicep\armconverted\*.json
+Get-ChildItem -Path .\Bicep -Filter *.bicep -Recurse | foreach { az bicep build --file $_.FullName --outfile (".\Bicep\armconverted\"+$_.BaseName+"_"+(Get-Random)+".json") }
